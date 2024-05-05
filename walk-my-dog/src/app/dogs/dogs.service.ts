91df7f0e924a9e903cee47eda2857d80e8c8e519
Diego Moreno <pelukron@users.noreply.github.com>
2024-05-04 18:37:43 -0600

    feat: bind observables from service to signals

--- walk-my-dog/src/app/dogs/dogs.service.ts
+++ walk-my-dog/src/app/dogs/dogs.service.ts
@@ -6,30 +6,41 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { Injectable } from '@angular/core';
-import { Subject, combineLatest, debounceTime, distinctUntilChanged, filter, map, of } from 'rxjs';
-import { DOGS } from './dog.model';
+import { Injectable, Signal, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Observable, Subject, combineLatest, debounceTime, distinctUntilChanged, interval, map, of } from 'rxjs';
+
+import { DOGS, Dog } from './dog.model';
 
 @Injectable({ providedIn: 'root' })
 export class DogsService {
-  
-  public readonly dogs$ = of(DOGS);
 
-  private _dogName$ = new Subject<string>();
-  public readonly dogName$ = this._dogName$.asObservable().pipe(
-    debounceTime(1000),
-    distinctUntilChanged()
-  );
+  private _dogs$: Observable<Dog[]> = of(DOGS);
+  private _dogNameSubject$: Subject<string> = new Subject<string>();
+  private _dogName$: Observable<string> = this._dogName();
+  private _filterList$: Observable<Dog[]> = this._filterList();
 
-  public filterList$ = combineLatest([this.dogs$, this.dogName$]).pipe(
-    map(([dogs, dogName]) => {
-      const dog = dogs.find(dog => dog.name.search(dogName));
-      return dog ? [dog] : []
-    })
-  );
+  public readonly dogsList: Signal<Dog[]> = toSignal<Dog[], Dog[]>(this._dogs$, { initialValue: [] });
+  public readonly filterList: Signal<Dog[]> = toSignal(this._filterList$, { initialValue: [] as Dog[] });
 
   public filterDog(dogName: string): void {
-    this._dogName$.next(dogName);
+    this._dogNameSubject$.next(dogName);
+  }
+
+  private _dogName(): Observable<string> {
+    return this._dogNameSubject$.asObservable().pipe(
+      debounceTime(1000),
+      distinctUntilChanged()
+    )
+  }
+
+  private _filterList(): Observable<Dog[]> {
+    return combineLatest([this._dogs$, this._dogName$]).pipe(
+      map(([dogs, dogName]) => {
+        const dog = dogs.filter(dog => dog.name.search(dogName));
+        return dog ? dog : []
+      })
+    );
   }
 
 }
